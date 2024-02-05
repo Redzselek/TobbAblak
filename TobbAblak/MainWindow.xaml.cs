ebbee2569ffd1dd390a0bae33c0e94af3a242f44
0tac-egyrob <0tac-egyrob@SZT03>
2024-02-05 09:52:52 +0100

    Add project files.

---
+++ TobbAblak/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TobbAblak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {  
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UjAblak_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 ujablak = new Window1(FoAblak.Text);
+            UjAblak.IsEnabled = false;
+            ujablak.Show();
+            Hide();
+        }
+    }
+}
